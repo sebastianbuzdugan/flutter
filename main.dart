270b0f1f940289f60aa2a581c8a28e1d03f599d6
sebastianbuzdugan <81054605+sebastianbuzdugan@users.noreply.github.com>
2021-06-08 20:29:25 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:bmi_calculator/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
